a7252fec558a189a9fcfdd79468d54ef5f0d9e49
niquolic <nicolas.debruycker22@gmail.com>
2023-01-23 23:48:27 +0100

    ajout de css

--- film-search/src/App.vue
+++ film-search/src/App.vue
@@ -1,11 +1,49 @@
 <template>
     <header>
         <router-link to ="/">
-
+            <h1><span>Vue</span>Movies</h1>
         </router-link>
     </header>
-    <router-view/>
+    <main>
+        <router-view/>
+    </main>
 </template>
 
 <style lang="scss">
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: 'Segoe UI', sans-sherif;
+
+        &::selection {
+            background: transparentize($color: #42BAA3, $amount: 0)
+        }
+    }
+
+    body {
+        background-color: #35495E;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 10px 16px;
+        background-color: #2C3D4E;
+        box-shadow: 0px 0px 0px rgba(0, 0, 0, 0.1);
+
+        h1 {
+        color: #FFFFFF;
+        font-size: 28px;
+
+            span {
+                color: #42B883;
+            }
+        }
+    }
 </style>
